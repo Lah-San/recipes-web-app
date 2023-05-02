2540888d1a29267759f7b38341ea53a2deaebe44
Lah-San <lahirusandun14@gmail.com>
2023-05-03 02:50:17 +0530

    eight commit

--- src/pages/ImageLoader.jsx
+++ src/pages/ImageLoader.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export default function ImageLoader({ src, alt }) {
   return (
-    <ImageWrapper>
-      <LazyLoadImage src={src} alt={alt} effect="blur" />
-    </ImageWrapper>
+      <Image src={src} alt={alt} />
   );
 }
 
-const ImageWrapper = styled.div`
+const Image = styled.img`
   width: 100%;
   height: 100%;
+  object-fit: cover;
 `;
